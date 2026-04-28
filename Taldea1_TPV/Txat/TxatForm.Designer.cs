5115d4c35172ff0ae3eaf591e761a8ab44f69763
Alain Afonso Redondo <alainafonsoredondo@gmail.com>
2026-04-28 15:42:09 +0200

    Aldaketak

--- Taldea1_TPV/Txat/TxatForm.Designer.cs
+++ Taldea1_TPV/Txat/TxatForm.Designer.cs
@@ -122,7 +122,7 @@
             this.MaximizeBox = false;
             this.Name = "TxatForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Txata Profesionala";
+            this.Text = "AJA - Txata";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TxatForm_FormClosing);
             this.Load += new System.EventHandler(this.TxatForm_Load);
             this.pnlSarrera.ResumeLayout(false);
